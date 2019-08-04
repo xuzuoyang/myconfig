fe5a8cf7b24bf7076aabd415933244f918061fc4
xuzuoyang <xuzuoyang@gmail.com>
2019-08-04 21:15:13 +0800

    add ss script

---
+++ scripts/sssetup.sh
@@ -0,0 +1,4 @@
+wget --no-check-certificate -O shadowsocks.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks.sh
+chmod +x shadowsocks.sh
+wget — no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+chmod +x bbr.sh
